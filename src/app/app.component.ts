13853b79612bed604425eb3d9dc268d509ff9abe
Sergey Sotnikov <ssotnikov1990@gmail.com>
2021-11-23 13:10:49 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,53 @@
 import { Component } from '@angular/core';
 
+
+
+class Language {
+  #code:any='en';
+  #name:any='english';
+  #rtl:boolean=false;
+  #default:boolean=false;
+
+  constructor(property:any){
+    this.#code=property.code;
+    this.#name=property.name;
+    this.#rtl=property.rtl;
+    this.#default=property.default;
+  }
+  get  codeInfo() {
+    return this.#code;
+  }
+  get nameInfo() {
+    return this.#name
+  }
+  get isRTL() {
+    return this.#rtl
+  }
+  get isDefault() {
+    return this.#default
+  }
+
+}
+
+let lang1= new Language({
+  code:'ru',
+  name:'russia',
+  rtl :true,
+  default:true
+});
+
+console.log(lang1.isDefault);
+
+
+
+
+
+
+
+
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
